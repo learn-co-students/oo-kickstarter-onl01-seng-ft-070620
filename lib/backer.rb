f75903757f26de627efa2ba5ea5d9d3873f4b2ba
Amanda_Katherine <walker.akw@gmail.com>
2020-07-21 14:34:02 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
 
     def back_project(project)
         @backed_projects << project
-        project.backers << self
+        project.backers << project
     end
 
     # binding.pry
